7430d6e23c8c349efd4b89df5b7e0c2a69ab8ef0
noob9527 <2061864@qq.com>
2020-06-13 16:39:20 +0800

    init commit

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>hello</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
